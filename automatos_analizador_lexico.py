f861b94258543e52e8893cb6fd652eaf1adcb644
Juscelino Dias <56329683+juscelinodias5@users.noreply.github.com>
2023-05-23 16:26:06 -0300

    refactor -  retornando o token

--- automatos_analizador_lexico.py
+++ automatos_analizador_lexico.py
@@ -9,65 +9,47 @@ SIMBOLOS_ESPECIAIS = [';', ',', '.', '+', '-', '*',
 # indentificadores
 # EX: exemplo de alguns identificadores válidos: i , jx , z_teste , k12aa3, u_teste123 , u_12tes3, ab#sd, a9#sd
 # exemplo de alguns identificadores inválidos: z_b_bb_ , hhz_nota, 2abc, #abc, asd#, W#D#Df, 2#rt,
-identificadores_validos = ['i', 'jx', 'z_teste',
-                           'k12aa3', 'u_teste123', 'u_12tes3', 'ab#sd', 'a9#sd']
-identificadores_invalidos = ['z_b_bb_', 'hhz_nota',
-                             '2abc', '#abc', 'asd#', 'W#D#Df', '2#rt']
 
 
 def identificadores(palavra):
     estado = 0
-
+    token = ''
+    aux = ''  # recebe o caracter válido em um estado não final
     for simbolo in palavra:
         if estado == 0 and simbolo in LETRAS:
             estado = 1
-        elif estado == 1:
-            if simbolo in LETRAS or simbolo in DIGITOS:
-                estado = 3
-            elif simbolo == "_" or simbolo == "#":
-                estado = 2
-            else:
-                estado = None
-                break
-        elif estado == 2:
-            if simbolo in LETRAS or simbolo in DIGITOS:
-                estado = 5
-            else:
-                estado = None
-                break
-        elif estado == 3:
-            if simbolo in LETRAS or simbolo in DIGITOS:
-                estado = 3
-            elif simbolo == "#":
-                estado = 4
-            else:
-                estado = None
-                break
-        elif estado == 4:
-            if simbolo in LETRAS or simbolo in DIGITOS:
-                estado = 5
-            else:
-                estado = None
-                break
-        elif estado == 5:
-            if simbolo in LETRAS or simbolo in DIGITOS:
-                estado = 5
-            else:
-                estado = None
-                break
+            token += simbolo
+        elif estado == 1 and (simbolo in LETRAS or simbolo in DIGITOS):
+            estado = 3
+            token += simbolo
+        elif estado == 1 and simbolo in ('_', '#'):
+            estado = 2
+            aux += simbolo
+        elif estado == 3 and (simbolo in LETRAS or simbolo in DIGITOS):
+            estado = 3
+            token += simbolo
+        elif estado == 3 and simbolo == "#":
+            estado = 4
+            aux += simbolo
+        elif estado in (2, 4, 5) and (simbolo in LETRAS or simbolo in DIGITOS):
+            estado = 5
+            token += aux
+            aux = ''
+            token += simbolo
         else:
-            estado = None
-            break
+            return token
 
-    if estado == 1 or estado == 5 or estado == 3:
-        print("Identificador")
-    else:
-        print("cadeia não reconhecida")
+    return token
 
 
-# for palavra in identificadores_validos:
-#     identificadores(palavra)
+# if __name__ == "__main__":
+#     identificadores_validos = ['i', 'jx', 'z_teste',
+#                                'k12aa3', 'u_teste123', 'u_12tes3', 'ab#sd', 'a9#sd']
+#     identificadores_invalidos = ['z_b_bb_', 'hhz_nota',
+#                                  '2abc', '#abc', 'asd#', 'W#D#Df', '2#rt']
 
+#     for identificador in identificadores_invalidos:
+#         print(identificadores(identificador))
 
 #  Automato para Palavras reservadas
 #  program, if, then, else, while, do, until, repeat, int, double, char, case, switch, end, procedure, function,
@@ -104,60 +86,50 @@ def palavras_reservadas(palavra):
 
 def simbolos_especiais(palavra):
     estado = 0
-
+    token = ''
     for simbolo in palavra:
         if estado == 0:
             if simbolo in SIMBOLOS_ESPECIAIS:
                 estado = 1
+                token += simbolo
             elif simbolo in (":", ">"):
                 estado = 2
+                token += simbolo
             elif simbolo == "<":
                 estado = 3
+                token += simbolo
             elif simbolo == "*":
                 estado = 4
+                token += simbolo
             elif simbolo == "+":
                 estado = 5
-        elif estado == 2:
-            if simbolo == "=":
-                estado = 1
-            else:
-                estado = None
-                break
-        elif estado == 3:
-            if simbolo in ("=", ">"):
-                estado = 1
-            else:
-                estado = None
-                break
-        elif estado == 4:
-            if simbolo == "*":
-                estado = 1
-            else:
-                estado = None
-                break
-        elif estado == 5:
-            if simbolo == "+":
-                estado = 1
-            else:
-                estado = None
-                break
+                token += simbolo
+        elif estado == 2 and simbolo == '=':
+            estado = 1
+            token += simbolo
+        elif estado == 3 and simbolo in ('=', '>'):
+            estado = 1
+            token += simbolo
+        elif estado == 4 and simbolo == '*':
+            estado = 1
+            token += simbolo
+        elif estado == 5 and simbolo == '+':
+            estado = 1
+            token += simbolo
         else:
-            estado = None
-            break
+            return token
 
-    if estado in (1, 2, 3, 4, 5):
-        print("Simbolo especial")
-    else:
-        print("cadeia não reconhecida")
+    return token
 
 
-simbolos_validos = [';', ',', '.', '+', '-', '*',
-                    '(', ')', '<', '>', ':', '=', '{', '}', '/', '@', '#']
-simbolos_invalidos = [';;', ',,', '...', '<<',
-                      '>>', '<<=', '>>=', '7', 's', '&', '%']
+# if __name__ == "__main__":
+#     simbolos_validos = [';', ',', '.', '+', '-', '*',
+#                         '(', ')', '<', '>', ':', '=', '{', '}', '/', '@', '#']
+#     simbolos_invalidos = [';;', ',,', '...', '<<',
+#                           '>>', '<<=', '>>=', '7', 's', '&', '%', 'wea', '//']
 
-# for palavra in simbolos_validos:
-#     simbolos_especiais(palavra)
+#     for palavra in simbolos_invalidos:
+#         print(simbolos_especiais(palavra))
 
 
 # Reconhecer e tratar comentários:
@@ -165,75 +137,76 @@ simbolos_invalidos = [';;', ',,', '...', '<<',
 
 def comentarios(palavra):
     estado = 0
-
+    token = ''
     for simbolo in palavra:
         if estado == 0:
             if simbolo == "#":
                 estado = 3
+                token += simbolo
             elif simbolo == "@":
                 estado = 1
+                token += simbolo
             elif simbolo == "/":
                 estado = 6
+                token += simbolo
             else:
-                estado = None
-                break
-        elif estado == 1:
-            if simbolo == "@":
-                estado = 2
-            else:
-                estado = None
-                break
+                return
+        elif estado == 1 and simbolo == "@":
+            estado = 2
+            token += simbolo
         elif estado == 2:
             if simbolo == '\n':
-                estado = 9
+                token += simbolo
+                return token
             else:
                 estado = 2
-        elif estado == 3:
-            if simbolo == "#":
-                estado = 4
-            else:
-                estado = None
-                break
+                token += simbolo
+        elif estado == 3 and simbolo == "#":
+            estado = 4
+            token += simbolo
         elif estado == 4:
             if simbolo == "#":
                 estado = 5
+                token += simbolo
             else:
                 estado = 4
+                token += simbolo
         elif estado == 5:
             if simbolo == "#":
-                estado = 9
+                token += simbolo
+                return token
             else:
                 estado = 4
-        elif estado == 6:
-            if simbolo == "/":
-                estado = 7
-            else:
-                estado = None
-                break
+                token += simbolo
+        elif estado == 6 and simbolo == "/":
+            estado = 7
+            token += simbolo
         elif estado == 7:
             if simbolo == "/":
                 estado = 8
-            else :
-                estado = 7
-        elif estado == 8:
-            if simbolo == "/":
-                estado = 9
+                token += simbolo
             else:
                 estado = 7
+                token += simbolo
+        elif estado == 8:
+            if simbolo == "/":
+                token += simbolo
+                return token
+            else:
+                estado = 7
+                token += simbolo
         else:
-            estado = None
-            break
+            return
 
-    if estado == 9:
-        print("Comentário")
-    else:
-        print("cadeia não reconhecida")
 
-comentarios_validos = [ "@@comentario \n", "//com54* ss//", "//as/ddsa//", "##comentario##", "##com #as; d##"]
-comentarios_invalidos = ["##comentario\n","##comasd", "@@comentari", "//com54* ss/", "//as/ddsa/"]
+# if __name__ == "__main__":
+#     comentarios_validos = ["@@comentario \n", "//com54* ss//",
+#                            "//as/ddsa//", "##comentario##", "##com #as; d##"]
+#     comentarios_invalidos = ["##comentario\n", "##comasd",
+#                              "@@comentari", "//com54* ss/", "//as/ddsa/"]
 
-for palavra in comentarios_invalidos:
-    comentarios(palavra)
+#     for palavra in comentarios_invalidos:
+#         print(comentarios(palavra))
 
 # Reconhecer conforme exemplo:
 # 2, 345, 23.5, 5.55, -4.5
@@ -241,51 +214,43 @@ for palavra in comentarios_invalidos:
 
 def numeros(palavra):
     estado = 0
-
+    token = ''
+    aux = ''
     for simbolo in palavra:
-        if estado == 0:
-            if simbolo in DIGITOS:
-                estado = 1
-            elif simbolo == "-":
-                estado = 2
-        elif estado == 1:
-            if simbolo in DIGITOS:
-                estado = 1
-            elif simbolo == ".":
-                estado = 3
-            else:
-                estado = None
-                break
-        elif estado == 2:
-            if simbolo in DIGITOS:
-                estado = 1
-            else:
-                estado = None
-                break
-        elif estado == 3:
-            if simbolo in DIGITOS:
-                estado = 4
-            else:
-                estado = None
-                break
-        elif estado == 4:
-            if simbolo in DIGITOS:
-                estado = 4
-            else:
-                estado = None
-                break
+        if estado == 0 and simbolo in DIGITOS:
+            estado = 1
+            token += simbolo
+        elif estado == 0 and simbolo == "-":
+            estado = 2
+            aux += simbolo
+        elif estado == 1 and simbolo in DIGITOS:
+            estado = 1
+            token += simbolo
+        elif estado == 1 and simbolo == ".":
+            estado = 3
+            aux += simbolo
+        elif estado == 2 and simbolo in DIGITOS:
+            estado = 1
+            token += aux
+            aux = ''
+            token += simbolo
+        elif estado == 3 and simbolo in DIGITOS:
+            estado = 4
+            token += aux
+            aux = ''
+            token += simbolo
+        elif estado == 4 and simbolo in DIGITOS:
+            estado = 4
+            token += simbolo
         else:
-            estado = None
-            break
+            return token
 
-    if estado in (1, 4):
-        print("Número")
-    else:
-        print("cadeia não reconhecida")
+    return token
 
 
-numeros_validos = ["2", "345", "23.5", "5.55", "-4.5"]
-numeros_invalidos = ["2.", "2.3.4", "3-5", "3.4-"]
+# if __name__ == "__main__":
+#     numeros_validos = ["2", "345", "23.5", "5.55", "-4.5"]
+#     numeros_invalidos = ["2.", "2.3.4", "3-5", "-3.4-"]
 
-# for palavra in numeros_validos:
-#     numeros(palavra)
+#     for palavra in numeros_invalidos:
+#         print(numeros(palavra))
--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from automatos_analizador_lexico import identificadores, palavras_reservadas, nu
 
 
 def ler_arquivo(nome_arquivo):
+    token = ''
+
     with open(nome_arquivo, "r", encoding='utf') as arquivo:
         palavra = arquivo.read()
-        
+        while palavra:
+            if identificadores(palavra):
+                token = identificadores(palavra)
+                # list_identif.append(token)
+                print('Identificador: '+token)
+                palavra = palavra[len(token):]
+            else:
+                palavra = palavra[1:]
 
 
+list_identif = []
 ler_arquivo("arquivo.txt")
+# print(list_identif)
